332a1dbd83fb1324e9bbc280943235372236e05f
程祥贺 <chengxianghe@100tal.com>
2019-07-31 19:30:42 +0800

    个人中心 初始化

---
+++ pages/profile/childCpns/w-list-view/w-list-view.js
@@ -0,0 +1,28 @@
+// pages/profile/childCpns/w-list-view/w-list-view.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    list: {
+      type: Array,
+      value: Object
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    itemClick(e) {
+      console.log(e)
+    }
+  }
+})
